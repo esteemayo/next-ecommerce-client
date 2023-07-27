cf5e99a11ee91a08c0dff4e4fa6b92136a54e6d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 00:45:25 +0100

    fix: add navigation menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,16 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Link href='/' passHref>Ecommerce</Link>
+      <nav>
+        <Link href='/' passHref>Home</Link>
+        <Link href='/products' passHref>All products</Link>
+        <Link href='/categories' passHref>Categories</Link>
+        <Link href='/account' passHref>Account</Link>
+        <Link href='/cart' passHref>Cart (0)</Link>
+      </nav>
+    </header>
   );
 };
 
