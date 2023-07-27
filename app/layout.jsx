76e502a297318eaf6c4a37b6f25081ac56676295
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 00:47:46 +0100

    fix: modify

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 import Header from '@/components/Header';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -18,5 +18,5 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
-  )
-}
+  );
+};
