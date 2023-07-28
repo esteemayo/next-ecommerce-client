1fdfe794396a1182628b8ff753a53f86a4f404b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 03:15:31 +0100

    fix: modify

--- components/PrimaryButton.jsx
+++ components/PrimaryButton.jsx
@@ -2,14 +2,12 @@
 
 import styled from 'styled-components';
 
-const PrimaryButton = () => {
-  return (
-    <Button type='button'>PrimaryButton</Button>
-  );
+const PrimaryButton = ({ children }) => {
+  return <Button type='button'>{children}</Button>;
 };
 
 const Button = styled.button`
-  
+  background-color: #5542f6;
 `;
 
 export default PrimaryButton;
