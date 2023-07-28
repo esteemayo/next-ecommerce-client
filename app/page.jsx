d88b80618426aa91d05530e99ad5ad4ad62355ea
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 01:59:15 +0100

    fix: modify

--- app/page.jsx
+++ app/page.jsx
@@ -4,26 +4,11 @@ import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <Container>
-      <Heading>Pro anywhere</Heading>
-      <Description>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam velit corrupti
-        vitae eius consequuntur sint quam alias expedita ipsa eum ut at, aut animi, quo reiciendis
-        deleniti totam. Minus!
-      </Description>
-    </Container>
+    <Container>Home</Container>
   );
 };
 
 const Container = styled.main`
-  
-`;
-
-const Heading = styled.h1`
-  
-`;
-
-const Description = styled.p`
   
 `;
 
