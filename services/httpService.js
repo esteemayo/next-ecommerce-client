5c9516fa82033ba4dacfe4ee7e7ea5c95f444237
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 14:54:15 +0100

    feat: create http service

---
+++ services/httpService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+import logger from './logService';
+
+const API = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+API.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status > 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+    toast.error('An unexpected error occurred');
+
+    return Promise.reject(error);
+  }
+});
+
+const http = {
+  get: API.get,
+  post: API.post,
+  patch: API.patch,
+  delete: API.delete,
+};
+
+export default http;
