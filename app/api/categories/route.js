8d780145fdd7aa26abdd230ae25ce84d4559e36c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 16:21:22 +0100

    fix: modify

---
+++ app/api/categories/route.js
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+
+import connectDB from '@/utils/db';
+import getIsAdmin from '@/actions/getIsAdmin';
+import Category from '@/models/Category';
+
+export const GET = async (request) => {
+  try {
+    await connectDB();
+    const isAdmin = await getIsAdmin();
+
+    if (isAdmin) {
+      const categories = await Category.find().sort('-createdAt');
+
+      return NextResponse.json(categories, {
+        status: 200,
+      });
+    }
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request) => {
+  const body = await request.json();
+
+  try {
+    await connectDB();
+    const isAdmin = await getIsAdmin();
+
+    if (isAdmin) {
+      const category = await Category.create({ ...body });
+
+      if (category) {
+        return NextResponse.json(category, {
+          status: 201,
+        });
+      }
+    }
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
