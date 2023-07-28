7d00f95e86e5a5f75655adae7cc754d7b48e6a17
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 03:13:10 +0100

    feat: create PrimaryButton component

---
+++ components/PrimaryButton.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import styled from 'styled-components';
+
+const PrimaryButton = () => {
+  return (
+    <Button type='button'>PrimaryButton</Button>
+  );
+};
+
+const Button = styled.button`
+  
+`;
+
+export default PrimaryButton;
