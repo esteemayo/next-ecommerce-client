35d1bc4b79e1e51f1393a9fa2f3f002ad3e3352c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 03:40:27 +0100

    fix: style button element

--- components/Button.jsx
+++ components/Button.jsx
@@ -13,11 +13,14 @@ const Button = ({ children, ...rest }) => {
 const StyledButton = styled.button`
   border: none;
   display: inline-block;
-  background-color: #5542f6;
   color: #fff;
   padding: 0.5rem 1.5rem;
   border-radius: 0.5rem;
   cursor: pointer;
+
+  ${({ primary }) => primary && css`
+    background-color: #5542f6;
+  `}
 
   ${({ size }) => size === 'lg' && css`
     font-size: 2rem;
