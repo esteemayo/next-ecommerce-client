f88007d18365ecabd23a1b487f0a3f7b43aee5cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 03:20:04 +0100

    fix: style button element

--- components/PrimaryButton.jsx
+++ components/PrimaryButton.jsx
@@ -10,6 +10,9 @@ const Button = styled.button`
   border: none;
   display: inline-block;
   background-color: #5542f6;
+  color: #fff;
+  padding: 0.5rem 1.5rem;
+  border-radius: 0.5rem;
 `;
 
 export default PrimaryButton;
