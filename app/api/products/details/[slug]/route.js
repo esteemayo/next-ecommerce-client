0073e467fafeadbc0f34de2531ec6a540a63cfa3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 16:22:05 +0100

    fix: modify

---
+++ app/api/products/details/[slug]/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+
+import connectDB from '@/utils/db';
+import getIsAdmin from '@/actions/getIsAdmin';
+import Product from '@/models/Product';
+
+export const GET = async (request, { params }) => {
+  const { slug } = params;
+
+  try {
+    await connectDB();
+    const isAdmin = await getIsAdmin();
+
+    if (isAdmin) {
+      const product = await Product.findOne({ slug });
+
+      if (!product) {
+        throw new Error('No product found with the given ID');
+      }
+
+      return NextResponse.json(product, {
+        status: 200,
+      });
+    }
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
