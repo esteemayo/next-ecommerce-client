5290e626885e394dcbbc7e2b823932f9eb6d265b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 03:34:01 +0100

    fix: rename component

---
+++ components/Button.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import styled, { css } from 'styled-components';
+
+const Button = ({ children, ...rest }) => {
+  return (
+    <StyledButton {...rest} type='button'>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  border: none;
+  display: inline-block;
+  background-color: #5542f6;
+  color: #fff;
+  padding: 0.5rem 1.5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+
+  ${({ size }) => size === 'lg' && css`
+    font-size: 2rem;
+    padding: 1rem 2rem;
+  `}
+`;
+
+export default Button;
