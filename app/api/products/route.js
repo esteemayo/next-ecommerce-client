d6f209b1dee18fe33511d1a646a384eba2ddecb6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 00:42:36 +0100

    fix: modify

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,21 +1,17 @@
 import { NextResponse } from 'next/server';
 
 import connectDB from '@/utils/db';
-import getIsAdmin from '@/actions/getIsAdmin';
 import Product from '@/models/Product';
 
 export const GET = async (request) => {
   try {
     await connectDB();
-    const isAdmin = await getIsAdmin();
 
-    if (isAdmin) {
-      const products = await Product.find().sort('-createdAt');
+    const products = await Product.find().sort('-createdAt');
 
-      return NextResponse.json(products, {
-        status: 200,
-      });
-    }
+    return NextResponse.json(products, {
+      status: 200,
+    });
   } catch (err) {
     return NextResponse.json(err.message, {
       status: 500,
