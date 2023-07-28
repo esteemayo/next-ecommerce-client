1d276d4b2f0225a3d1a3db83f918a5053fa2b54d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-28 01:57:57 +0100

    feat: create Featured component

---
+++ components/Featured.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Featured = () => {
+  return (
+    <Container>
+      <Heading>Pro anywhere</Heading>
+      <Description>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam velit corrupti
+        vitae eius consequuntur sint quam alias expedita ipsa eum ut at, aut animi, quo reiciendis
+        deleniti totam. Minus!
+      </Description>
+    </Container>
+  );
+};
+
+const Container = styled.main`
+  
+`;
+
+const Heading = styled.h1`
+  
+`;
+
+const Description = styled.p`
+  
+`;
+
+export default Featured;
