8b95b831ad182215ae06d71799ae30a9e1477864
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 02:50:45 +0100

    fix: add styling

--- components/ProductBox.jsx
+++ components/ProductBox.jsx
@@ -5,13 +5,22 @@ import Image from 'next/image';
 
 const ProductBox = ({ title, slug, description, price, images }) => {
   return (
-    <Container>ProductBox</Container>
+    <Container>
+      <Image src={images[0]} fill alt='' />
+    </Container>
   );
 };
 
 const Container = styled.article`
   background-color: var(--clr-white);
   padding: 2rem;
+  position: relative;
+
+  img {
+    max-width: 100%;
+    max-height: 15rem;
+    object-fit: cover;
+  }
 `;
 
 export default ProductBox;
