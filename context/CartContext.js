3fce4962c47bfe64b875d6c2013575e7487417ae
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 17:58:39 +0100

    fix: add cartKey

--- context/CartContext.js
+++ context/CartContext.js
@@ -3,7 +3,7 @@ import { createContext, useContext, useEffect, useReducer } from 'react';
 import CartReducer from './CartReducer';
 import { ADD_TO_CART } from './CartTypes';
 
-import { setToStorage } from '@/utils';
+import { cartKey, setToStorage } from '@/utils';
 
 const INITIAL_STATE = {
   cart: [],
