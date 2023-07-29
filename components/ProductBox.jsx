0d46d39deb3200cc0a55061f4bb6f6ff7bd00ed6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 04:54:35 +0100

    fix: add styling

--- components/ProductBox.jsx
+++ components/ProductBox.jsx
@@ -37,7 +37,7 @@ const ProductBox = ({
               prefix={'$'}
             />
           </Price>
-          <Button type='button' primary={1}>
+          <Button type='button' primary={1} outline={1}>
             <CartIcon />
           </Button>
         </PriceContainer>
