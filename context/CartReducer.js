a2b9e4358a1a3d0e6153d303d73b5997f9a9564f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 16:51:54 +0100

    fix: handle add to cart functionality

--- context/CartReducer.js
+++ context/CartReducer.js
@@ -1,7 +1,16 @@
 import { ADD_TO_CART } from './CartTypes';
 
 const CartReducer = (state, { payload, type }) => {
-  // 
+  switch (type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, payload],
+      };
+
+    default:
+      return state;
+  };
 };
 
 export default CartReducer;
