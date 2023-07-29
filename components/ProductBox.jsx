1dcc52ebda3597f0a33d562b1636fef85e3b0834
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 04:04:23 +0100

    fix: modify

--- components/ProductBox.jsx
+++ components/ProductBox.jsx
@@ -8,7 +8,7 @@ const ProductBox = ({ title, slug, description, price, images }) => {
     <Container>
       <Wrapper>
         <ImageWrapper>
-          <Image src={images[0]} fill alt='' />
+          <Image src={images[0]} width={175} height={80} alt='' />
         </ImageWrapper>
       </Wrapper>
       <Title>{title}</Title>
@@ -23,19 +23,17 @@ const Container = styled.article`
 const Wrapper = styled.div`
   background-color: var(--clr-white);
   padding: 2rem;
-  width: 100%;
   height: 12rem;
   text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 1rem;
-  position: relative;
 
   img {
     max-width: 100%;
     max-height: 8rem;
-    object-fit: contain;
+    object-fit: cover;
   }
 `;
 
