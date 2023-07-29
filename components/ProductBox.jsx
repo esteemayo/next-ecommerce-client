678cc5a8235ff244dd8328e99a354a2016d1af5a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 03:17:10 +0100

    fix: add styling

--- components/ProductBox.jsx
+++ components/ProductBox.jsx
@@ -21,11 +21,13 @@ const Container = styled.article`
 const Wrapper = styled.div`
   background-color: var(--clr-white);
   padding: 2rem;
+  width: 100%;
+  height: 15rem;
   position: relative;
 
   img {
-    width: 100%;
-    height: 15rem !important;
+    max-width: 100%;
+    max-height: 100%;
     object-fit: cover;
   }
 `;
