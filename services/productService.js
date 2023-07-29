89dc4673fff9f2e89516a646a40df103620419fc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-29 02:00:56 +0100

    fix: add getNewProducts function

--- services/productService.js
+++ services/productService.js
@@ -2,5 +2,7 @@ import http from './httpService';
 
 const apiEndpoint = '/products';
 
+export const getNewProducts = () => http.get(`${apiEndpoint}/new`);
+
 export const getProductById = (productId) =>
   http.get(`${apiEndpoint}/${productId}`);
