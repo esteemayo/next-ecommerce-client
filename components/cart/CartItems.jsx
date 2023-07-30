ca530c96ada8c84974e5e684312ee7d4febc2225
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-30 22:54:29 +0100

    fix: modify

--- components/cart/CartItems.jsx
+++ components/cart/CartItems.jsx
@@ -3,11 +3,57 @@
 import styled from 'styled-components';
 import { NumericFormat } from 'react-number-format';
 import Image from 'next/image';
+
 import Table from '@/components/Table';
 
-const CartItems = () => {
+const CartItems = ({ cart, products }) => {
   return (
-    <Container>CartItems</Container>
+    <Container>
+      <Heading>Cart</Heading>
+      {!cart?.length && (
+        <EmptyMessage>Your cart is empty</EmptyMessage>
+      )}
+      {products?.length > 0 && (
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Products</Th>
+              <Th>Quantity</Th>
+              <Th>Price</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {products.map((item, index) => {
+              const { _id: id, title, price, images } = item;
+              return (
+                <Tr key={index}>
+                  <ProductInfoCell>
+                    <ImageContainer>
+                      <Image
+                        src={images[0]}
+                        width={80}
+                        height={80}
+                        alt=''
+                      />
+                    </ImageContainer>
+                    {title}
+                  </ProductInfoCell>
+                  <Td>{numberOfProducts(id)}</Td>
+                  <Td>
+                    <NumericFormat
+                      value={price}
+                      displayType={'text'}
+                      thousandSeparator={true}
+                      prefix={'$'}
+                    />
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      )}
+    </Container>
   );
 };
 
@@ -41,6 +87,27 @@ const Th = styled.th`
 
 const Td = styled.td`
   
+`;
+
+const ProductInfoCell = styled.td`
+  padding: 1rem 0;
+`;
+
+const ImageContainer = styled.div`
+  max-width: 10rem;
+  max-height: 10rem;
+  padding: 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-width: 8rem;
+    max-height: 8rem;
+    object-fit: cover;
+  }
 `;
 
 export default CartItems;
