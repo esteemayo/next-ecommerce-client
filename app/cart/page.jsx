f15d3a67ebde8cfa79247ae5c458bded44b7f0e8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-30 23:21:57 +0100

    fix: modify

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -9,7 +9,6 @@ import Center from '@/components/Center';
 
 import { getCarts } from '@/services/cartService';
 import { useGlobalContext } from '@/context/CartContext';
-import CartItems from '@/components/cart/CartItems';
 
 const Cart = () => {
   const { cart } = useGlobalContext();
