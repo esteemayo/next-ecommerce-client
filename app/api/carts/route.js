c8bbae774c01eebed8623b73dabf86a847f5aa82
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-30 02:22:46 +0100

    fix: modify

---
+++ app/api/carts/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+
+import connectDB from '@/utils/db';
+import Product from '@/models/Product';
+
+export const POST = async (request) => {
+  const { ids } = await request.json();
+
+  try {
+    await connectDB();
+
+    const products = await Promise.all(ids.map((id) => {
+      return Product.find({ _id: id });
+    }));
+
+    return NextResponse.json(products, {
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+}
