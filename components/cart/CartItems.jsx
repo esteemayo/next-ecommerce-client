46d44ba42e3e4fd9366913625ecd21de2e9d9ea3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-30 22:54:12 +0100

    fix: add next/image module

--- components/cart/CartItems.jsx
+++ components/cart/CartItems.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { NumericFormat } from 'react-number-format';
+import Image from 'next/image';
 import Table from '@/components/Table';
 
 const CartItems = () => {
