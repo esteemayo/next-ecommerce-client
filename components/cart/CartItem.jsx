823bb3fd93c1993d29a876b9b5c408dce5ba3c75
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-30 23:00:19 +0100

    fix: modify

--- components/cart/CartItem.jsx
+++ components/cart/CartItem.jsx
@@ -8,4 +8,37 @@ const CartItem = () => {
   );
 };
 
+const Tr = styled.tr`
+
+`;
+
+const Th = styled.th`
+
+`;
+
+const Td = styled.td`
+  
+`;
+
+const ProductInfoCell = styled.td`
+  padding: 1rem 0;
+`;
+
+const ImageContainer = styled.div`
+  max-width: 10rem;
+  max-height: 10rem;
+  padding: 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-width: 8rem;
+    max-height: 8rem;
+    object-fit: cover;
+  }
+`;
+
 export default CartItem;
