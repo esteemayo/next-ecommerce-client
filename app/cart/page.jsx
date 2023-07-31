3fea8843dee9e2b99dc51e3f53f0bb2f7fec67f6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-31 02:52:28 +0100

    fix: refactor

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 
 import Button from '@/components/Button';
 import ClientOnly from '@/components/ClientOnly';
