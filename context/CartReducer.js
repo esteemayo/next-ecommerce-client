9f0f49a68ad0337b8880c861b031c9fff7640cab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-31 14:55:14 +0100

    fix: add GET_TOTALS

--- context/CartReducer.js
+++ context/CartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, FETCH_CART_PRODUCT, REMOVE_FROM_CART } from './CartTypes';
+import { ADD_TO_CART, FETCH_CART_PRODUCT, GET_TOTALS, REMOVE_FROM_CART } from './CartTypes';
 
 const CartReducer = (state, { payload, type }) => {
   switch (type) {
