277c48ebaf16428a981a388ed1b61f3db006ed6a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-31 14:51:35 +0100

    fix: add GET_TOTALS

--- context/CartTypes.js
+++ context/CartTypes.js
@@ -1,3 +1,4 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const FETCH_CART_PRODUCT = 'FETCH_CART_PRODUCT';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const GET_TOTALS = 'GET_TOTALS';
