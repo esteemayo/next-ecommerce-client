88f7255f5b57e033796fc7c3ca738db99bff03e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-31 15:56:05 +0100

    fix: add NunericFilter react-number-format hook

--- components/cart/CartItems.jsx
+++ components/cart/CartItems.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { NumericFormat } from 'react-number-format';
 
 import CartItem from './CartItem';
 import Table from '@/components/Table';
