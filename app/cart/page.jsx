2f2171477b27955c18f16b1695ba30ea68d50b5c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-01 02:55:30 +0100

    fix: modify

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -120,7 +120,6 @@ const Cart = () => {
                   <Input
                     type='text'
                     name='streetAddress'
-                    value={streetAddress}
                     placeholder='Street Address'
                     onChange={handleChange}
                   />
