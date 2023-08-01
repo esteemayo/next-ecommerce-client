773f9b9f5de131f791f6eb2c65a21ced2a261375
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-01 16:40:26 +0100

    fix: add CLEAR_CART

--- context/CartTypes.js
+++ context/CartTypes.js
@@ -1,5 +1,6 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const FETCH_CART_PRODUCT = 'FETCH_CART_PRODUCT';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const CLEAR_CART = 'CLEAR_CART';
 export const CLEAR_CART_PRODUCT = 'CLEAR_CART_PRODUCT';
 export const GET_TOTALS = 'GET_TOTALS';
