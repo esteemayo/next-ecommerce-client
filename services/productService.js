8e8b46b84fd3bff62da33cb9d0eb18d75059e3ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-02 02:44:38 +0100

    fix: refactor

--- services/productService.js
+++ services/productService.js
@@ -2,9 +2,11 @@ import http from './httpService';
 
 const apiEndpoint = '/products';
 
+const productUrl = (productId) => `${apiEndpoint}/${productId}`;
+
 export const getProducts = () => http.get(apiEndpoint);
 
 export const getNewProducts = () => http.get(`${apiEndpoint}/new`);
 
 export const getProductById = (productId) =>
-  http.get(`${apiEndpoint}/${productId}`);
+  http.get(productUrl(productId));
