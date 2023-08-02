56004f909d297e71f93280ef77a113358b5e548a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-02 03:38:27 +0100

    fix: add useState react hook

--- app/products/[slug]/page.jsx
+++ app/products/[slug]/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Center from '@/components/Center';
 import ClientOnly from '@/components/ClientOnly';
