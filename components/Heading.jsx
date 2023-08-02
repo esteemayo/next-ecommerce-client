9d5ae257f430e8df5262a77d37ab569fdb65d967
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-02 03:33:08 +0100

    fix: modify

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,15 +1,7 @@
-'use client';
-
 import styled from 'styled-components';
 
-const Heading = () => {
-  return (
-    <div>Heading</div>
-  );
-};
-
-const Title = styled.h2`
-  
+const Heading = styled.h2`
+  font-size: 1.5em;
 `;
 
 export default Heading;
