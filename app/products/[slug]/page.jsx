6c8f1031cdf5b6556826d93c5d24bf67dfdc7d94
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-02 04:19:50 +0100

    fix: modify

--- app/products/[slug]/page.jsx
+++ app/products/[slug]/page.jsx
@@ -34,8 +34,6 @@ const Product = ({ params: { slug } }) => {
               <ImageContainer>
                 <Image
                   src={product?.images?.[0]}
-                  width={100}
-                  height={100}
                   alt=''
                 />
               </ImageContainer>
