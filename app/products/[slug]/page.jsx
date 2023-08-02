88ad1979938def65389dab88027c6c7996142c14
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-02 03:50:51 +0100

    fix: add Heading component

--- app/products/[slug]/page.jsx
+++ app/products/[slug]/page.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 
 import Center from '@/components/Center';
 import ClientOnly from '@/components/ClientOnly';
+import Heading from '@/components/Heading';
 
 import { getProductBySlug } from '@/services/productService';
 
