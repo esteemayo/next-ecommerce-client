92da3677745552d5c39a94ffdaa63675e5cc6ea1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-02 01:49:10 +0100

    feat: add navLinks array items

---
+++ data.js
@@ -0,0 +1,27 @@
+export const nevLinks = [
+  {
+    id: 1,
+    url: '/',
+    label: 'Home',
+  },
+  {
+    id: 2,
+    url: '/products',
+    label: 'All Products',
+  },
+  {
+    id: 3,
+    url: '/categories',
+    label: 'Categories',
+  },
+  {
+    id: 4,
+    url: '/account',
+    label: 'Account',
+  },
+  {
+    id: 5,
+    url: '/cart',
+    label: 'Cart',
+  },
+];
