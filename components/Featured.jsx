5abb426e20103ba0ea9b2e671ff94e3c56a7b572
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-03 15:33:29 +0100

    fix: add media query

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -98,7 +98,10 @@ const LeftWrapper = styled.div`
 
 const Heading = styled.h1`
   font-weight: normal;
-  font-size: 4rem;
+  
+  @media only screen and (min-width: 768px) {
+    font-size: 4rem;
+  }
 `;
 
 const Description = styled.p`
