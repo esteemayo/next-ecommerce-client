f2c8878df16003945214f6e4c05c56e8e63bc4a8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-03 01:07:18 +0100

    fix: add react-number-format module

--- app/products/[slug]/page.jsx
+++ app/products/[slug]/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
+import { NumericFormat } from 'react-number-format';
 import { useEffect, useState } from 'react';
 
 import Center from '@/components/Center';
