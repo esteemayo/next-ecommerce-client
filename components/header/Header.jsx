f5c0ab35854e3c9d5ef3f334bb84519f6356b4d5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-05 01:20:36 +0100

    fix: add Logo component

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { useCallback, useState } from 'react';
 
 import Center from '../Center';
+import Logo from './Logo';
 import MenuItem from './MenuItem';
 
 import BarsIcon from '../icons/BarsIcon';
