975f8acc8ded26f6467e94a824991e75033caca2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-06 22:38:01 +0100

    fix: add a div element tag

--- components/cart/CartInfo.jsx
+++ components/cart/CartInfo.jsx
@@ -9,50 +9,52 @@ import WhiteBox from '@/components/WhiteBox';
 const CartInfo = ({ onChange, onSubmit }) => {
   return (
     <WhiteBox>
-      <Heading>Order information</Heading>
-      <Form onSubmit={onSubmit}>
-        <Input
-          type='text'
-          name='name'
-          placeholder='Name'
-          onChange={onChange}
-        />
-        <Input
-          type='email'
-          name='email'
-          placeholder='Email'
-          onChange={onChange}
-        />
-        <CityHolder>
+      <Container>
+        <Heading>Order information</Heading>
+        <Form onSubmit={onSubmit}>
           <Input
             type='text'
-            name='city'
-            placeholder='City'
+            name='name'
+            placeholder='Name'
+            onChange={onChange}
+          />
+          <Input
+            type='email'
+            name='email'
+            placeholder='Email'
+            onChange={onChange}
+          />
+          <CityHolder>
+            <Input
+              type='text'
+              name='city'
+              placeholder='City'
+              onChange={onChange}
+            />
+            <Input
+              type='text'
+              name='postalCode'
+              placeholder='Postal Code'
+              onChange={onChange}
+            />
+          </CityHolder>
+          <Input
+            type='text'
+            name='streetAddress'
+            placeholder='Street Address'
             onChange={onChange}
           />
           <Input
             type='text'
-            name='postalCode'
-            placeholder='Postal Code'
+            name='country'
+            placeholder='Country'
             onChange={onChange}
           />
-        </CityHolder>
-        <Input
-          type='text'
-          name='streetAddress'
-          placeholder='Street Address'
-          onChange={onChange}
-        />
-        <Input
-          type='text'
-          name='country'
-          placeholder='Country'
-          onChange={onChange}
-        />
-        <Button type='submit' black={1} block={1}>
-          Continue to payment
-        </Button>
-      </Form>
+          <Button type='submit' black={1} block={1}>
+            Continue to payment
+          </Button>
+        </Form>
+      </Container>
     </WhiteBox>
   );
 };
